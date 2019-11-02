e0a24f31d4058066968baad4d569fec87d4dc8e8
alekseik1 <1alekseik1@gmail.com>
2019-11-03 00:52:23 +0300

    feat: convert for volume and pressure

--- units_converter.py
+++ units_converter.py
@@ -22,10 +22,18 @@ class SiAtomicConverter:
             return temperature_joule*physical_constants['joule-kelvin relationship'][0]
 
     def convert_volume(self, volume):
-        pass
+        if self.from_si:
+            return volume/physical_constants['atomic unit of length'][0]**3
+        else:
+            return volume*physical_constants['atomic unit of length'][0]**3
 
     def convert_pressure(self, pressure):
-        pass
+        if self.from_si:
+            return pressure/physical_constants['atomic unit of force'][0] * \
+                   physical_constants['atomic unit of length']**2
+        else:
+            return pressure*physical_constants['atomic unit of force'][0] / \
+                   physical_constants['atomic unit of length']**2
 
     def convert_entropy(self, entropy):
         pass
