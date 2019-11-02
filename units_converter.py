660ae0ee06eb0091d73cd90c6a7619a867642caa
alekseik1 <1alekseik1@gmail.com>
2019-11-03 00:41:55 +0300

    feat: units converter. Energy and temperature only

---
+++ units_converter.py
@@ -0,0 +1,37 @@
+from scipy.constants import physical_constants
+
+
+class SiAtomicConverter:
+
+    def __init__(self, from_si=True):
+        self.from_si = from_si
+
+    def convert_energy(self, energy):
+        if self.from_si:
+            return energy/physical_constants['atomic unit of energy'][0]
+        else:
+            return energy*physical_constants['atomic unit of energy'][0]
+
+    def convert_temperature(self, temperature):
+        if self.from_si:
+            temperature_joule = temperature*physical_constants['kelvin-joule relationship'][0]
+            return temperature_joule*physical_constants['joule-hartree relationship'][0]
+        else:
+            # E_h -> Joules -> Kelvins
+            temperature_joule = temperature*physical_constants['hartree-joule relationship'][0]
+            return temperature_joule*physical_constants['joule-kelvin relationship'][0]
+
+    def convert_volume(self, volume):
+        pass
+
+    def convert_pressure(self, pressure):
+        pass
+
+    def convert_entropy(self, entropy):
+        pass
+
+    def convert_heat_capacity(self, heat_capacity):
+        pass
+
+    def convert_sound_speed(self, sound_speed):
+        pass
