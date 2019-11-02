2c1e161eedb32dd1a48df69352c3bd8c5430534a
alekseik1 <1alekseik1@gmail.com>
2019-11-03 02:15:32 +0300

    feat: example

--- ifg.py
+++ ifg.py
@@ -1,5 +1,6 @@
 from fdint import fdk, ifd1h
 import numpy as np
+from units_converter import SiAtomicConverter
 
 
 def get_chemical_potential(specific_volume, temperature):
@@ -110,4 +111,106 @@ def get_sound_speed_entropy(specific_volume, temperature, chemical_potential):
 
 
 if __name__ == '__main__':
-    pass
+    import matplotlib.pyplot as plt
+    converter = SiAtomicConverter(from_si=True)
+    reverse_converter = SiAtomicConverter(from_si=False)
+    # For Aluminium
+    v_Al = converter.convert_density(density_sgs=2.70, molar_mass_sgs=26.98)
+    # Since Aluminium has 3 electrons on valence shell
+    v_Al /= 3
+    T_range = np.arange(10**2, 10**6, 100)
+    # Convert to atomic units
+    v_Al = converter.convert_volume(v_Al)
+    T_range = converter.convert_temperature(T_range)
+    # TODO: refactor to something like `GasDescriber` that will take care of all plots
+    ##################################################
+    # For mu
+    ##################################################
+    # Calculate mu
+    mu_range = get_chemical_potential(
+        v_Al,
+        T_range
+    )
+    plt.plot(T_range, mu_range)
+    plt.title(r'$\mu (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$\mu, [E_h]$')
+    plt.show()
+    ##################################################
+    # For pressure
+    ##################################################
+    # Calculate pressure
+    p_range = get_pressure(
+        temperature=T_range,
+        chemical_potential=mu_range
+    )
+    plt.plot(T_range, p_range)
+    plt.title(r'$p (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$p$, [atomic units]')
+    plt.show()
+    ##################################################
+    # For entropy
+    ##################################################
+    S_range = get_entropy(
+        specific_volume=v_Al,
+        temperature=T_range,
+        chemical_potential=mu_range
+    )
+    plt.plot(T_range, S_range)
+    plt.title(r'$S (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$S$')
+    plt.show()
+    ##################################################
+    # For C_V
+    ##################################################
+    C_V_range = get_heat_capacity_volume(
+        specific_volume=v_Al,
+        temperature=T_range,
+        chemical_potential=mu_range
+    )
+    plt.plot(T_range, C_V_range)
+    plt.title(r'$C_V (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$C_V$')
+    plt.show()
+    ##################################################
+    # For C_P
+    ##################################################
+    C_P_range = get_heat_capacity_pressure(
+        specific_volume=v_Al,
+        temperature=T_range,
+        chemical_potential=mu_range
+    )
+    plt.plot(T_range, C_P_range)
+    plt.title(r'$C_P (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$C_P$')
+    plt.show()
+    ##################################################
+    # For C_T
+    ##################################################
+    C_T_range = get_sound_speed_temperature(
+        specific_volume=v_Al,
+        temperature=T_range,
+        chemical_potential=mu_range
+    )
+    plt.plot(T_range, C_T_range)
+    plt.title(r'$C_T (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$C_T$, [atomic units]')
+    plt.show()
+    ##################################################
+    # For C_S
+    ##################################################
+    C_S_range = get_sound_speed_entropy(
+        specific_volume=v_Al,
+        temperature=T_range,
+        chemical_potential=mu_range
+    )
+    plt.plot(T_range, C_S_range)
+    plt.title(r'$C_S (v, T)$')
+    plt.xlabel('T, atomic units')
+    plt.ylabel(r'$C_S$, [atomic units]')
+    plt.show()
