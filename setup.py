3ce66abc93e4ade36bc3c9d7142540c0b282865c
alekseik1 <1alekseik1@gmail.com>
2019-12-29 21:05:48 +0300

    fix: `setup_requires`

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/alekseik1/ifg-py',
     packages=setuptools.find_packages(),
+    setup_requires=['fdint', 'numpy', 'scipy', 'pandas'],
     install_requires=[
         'fdint',
         'scipy',
