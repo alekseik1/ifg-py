06a475ad9a9c51a77ffa4803eb574dce4ff721d6
alekseik1 <1alekseik1@gmail.com>
2019-12-30 18:35:40 +0300

    docs: add missing empty line

--- ifg_py/utils.py
+++ ifg_py/utils.py
@@ -4,6 +4,7 @@ import os
 def dump_to_csv(filepath, data):
     """
     Dumps `data` to csv under `filepath`. Creates all necessary folders
+
     :param filepath: Path to file, like 'data/1.csv'
     :param data: Data to dump
     :return:
