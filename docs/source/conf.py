917e8bec15dae84849abb4191cb12e3547bbd708
alekseik1 <1alekseik1@gmail.com>
2019-12-30 20:46:13 +0300

    build: add exception for docs build script

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-from ifg_py.metadata import __version__
+from ifg_py.metadata import __version__     # noqa: E402
 
 
 # -- Project information -----------------------------------------------------
