47e3939289494a8085d39343ecd3905034301fa8
alekseik1 <1alekseik1@gmail.com>
2020-05-09 13:11:38 +0300

    fix: change numpy assert functions to `assert_allclose` since they are more stable

--- tests/unit/test_asymptotics.py
+++ tests/unit/test_asymptotics.py
@@ -32,37 +32,37 @@ class TestLowTemperaturesLimits:
     def test_entropy(self, temps, vols):
         with set_up(temps, vols) as (calculator, vv, tt):
             expected = beta * tt * vv**(2/3)
-            np.testing.assert_array_almost_equal(calculator.S, expected)
+            np.testing.assert_allclose(calculator.S, expected)
 
     @given(temperatures_st, volumes_st)
     def test_C_V(self, temps, vols):
         with set_up(temps, vols) as (calc, vv, tt):
             expected = beta * tt * vv**(2/3)
-            np.testing.assert_array_almost_equal(calc.C_V, expected)
+            np.testing.assert_allclose(calc.C_V, expected)
 
     @given(temperatures_st, volumes_st)
     def test_C_P(self, temps, vols):
         with set_up(temps, vols) as (calc, vv, tt):
             expected = beta * tt * vv**(2/3)
-            np.testing.assert_array_almost_equal(calc.C_P, expected)
+            np.testing.assert_allclose(calc.C_P, expected)
 
     @given(temperatures_st, volumes_st)
     def test_C_T(self, temps, vols):
         with set_up(temps, vols) as (calc, vv, tt):
             expected = np.sqrt(10/9 * A * vv**(-2/3) + beta/9 * tt**2 * vv**(2/3))
-            np.testing.assert_array_almost_equal(calc.C_T, expected)
+            np.testing.assert_allclose(calc.C_T, expected)
 
     @given(temperatures_st, volumes_st)
     def test_C_S(self, temps, vols):
         with set_up(temps, vols) as (calc, vv, tt):
             expected = np.sqrt(10/9 * A * vv**(2/3) + 5/9 * beta * tt**2 * vv**(2/3))
-            np.testing.assert_array_almost_equal(calc.C_S, expected)
+            np.testing.assert_allclose(calc.C_S, expected)
 
     @given(temperatures_st, volumes_st)
     def test_F_potential(self, temps, vols):
         with set_up(temps, vols) as (calculator, vv, tt):
             expected = A*vv**(-2/3) - beta/2 * tt**2 * vv**(2/3)
-            np.testing.assert_array_almost_equal(calculator.F, expected)
+            np.testing.assert_allclose(calculator.F, expected)
 
     @given(temperatures_st, volumes_st)
     def test_chemical_potential(self, temperatures, volumes):
@@ -70,14 +70,14 @@ class TestLowTemperaturesLimits:
             # According to the original article
             eps_F = (3*np.pi**2/(np.sqrt(2)*g*vv))**(2/3)
             expected = eps_F*(1 - np.pi**2 / 12 * (tt/eps_F)**2)
-            np.testing.assert_array_almost_equal(calculator.mu, expected)
+            np.testing.assert_allclose(calculator.mu, expected)
 
     @given(temperatures_st, volumes_st)
     def test_pressure(self, temperatures, volumes):
         with set_up(temperatures, volumes) as (calculator, vv, tt):
             # According to the original article
             expected = 2 / 3 * A * vv ** (-5 / 3) + beta / 3 * tt ** 2 * vv ** (2 / 3)
-            np.testing.assert_array_almost_equal(calculator.p, expected)
+            np.testing.assert_allclose(calculator.p, expected)
 
 
 class TestHighTemperaturesLimits:
