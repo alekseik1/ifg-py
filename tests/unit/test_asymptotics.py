697b02cfc21c5b669a9393163c4df9df6d5df24d
alekseik1 <1alekseik1@gmail.com>
2020-05-09 10:58:52 +0300

    tests: move setUp to context manager

--- tests/unit/test_asymptotics.py
+++ tests/unit/test_asymptotics.py
@@ -1,5 +1,6 @@
 import hypothesis.extra.numpy as st_numpy
 from hypothesis import strategies as st, given
+from contextlib import contextmanager
 import numpy as np
 from ifg_py import IfgCalculator
 
@@ -15,31 +16,26 @@ volumes_st = st_numpy.arrays(np.float, st.tuples(st.integers(1, 1)),
                              elements=st.floats(10., 100.))
 
 
+@contextmanager
+def set_up(temps, vols):
+    yield (IfgCalculator(
+        temperatures=temps, specific_volumes=vols,
+        input_in_si=False, output_in_si=False), *np.meshgrid(vols, temps))
+
+
 class TestLowTemperaturesLimits:
 
     @given(temperatures_st, volumes_st)
     def test_chemical_potential(self, temperatures, volumes):
-        calculator = IfgCalculator(
-            temperatures=temperatures,
-            specific_volumes=volumes,
-            input_in_si=False,
-            output_in_si=False
-        )
-        vv, tt = np.meshgrid(volumes, temperatures)
-        # According to the original article
-        eps_F = (3*np.pi**2/(np.sqrt(2)*g*vv))**(2/3)
-        expected = eps_F*(1 - np.pi**2 / 12 * (tt/eps_F)**2)
-        np.testing.assert_array_almost_equal(calculator.mu, expected)
+        with set_up(temperatures, volumes) as (calculator, vv, tt):
+            # According to the original article
+            eps_F = (3*np.pi**2/(np.sqrt(2)*g*vv))**(2/3)
+            expected = eps_F*(1 - np.pi**2 / 12 * (tt/eps_F)**2)
+            np.testing.assert_array_almost_equal(calculator.mu, expected)
 
     @given(temperatures_st, volumes_st)
     def test_pressure(self, temperatures, volumes):
-        calculator = IfgCalculator(
-            temperatures=temperatures,
-            specific_volumes=volumes,
-            input_in_si=False,
-            output_in_si=False
-        )
-        vv, tt = np.meshgrid(volumes, temperatures)
-        # According to the original article
-        expected = 2 / 3 * A * vv ** (-5 / 3) + beta / 3 * tt ** 2 * vv ** (2 / 3)
-        np.testing.assert_array_almost_equal(calculator.p, expected)
+        with set_up(temperatures, volumes) as (calculator, vv, tt):
+            # According to the original article
+            expected = 2 / 3 * A * vv ** (-5 / 3) + beta / 3 * tt ** 2 * vv ** (2 / 3)
+            np.testing.assert_array_almost_equal(calculator.p, expected)
