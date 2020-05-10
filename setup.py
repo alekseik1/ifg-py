b7dd489113ecd9ec52fddcd690eccc1150ba6254
alekseik1 <1alekseik1@gmail.com>
2020-05-10 11:35:25 +0300

    build: remove `setup_requires` section build: add classifiers for all supported python versions

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/alekseik1/ifg-py',
     packages=setuptools.find_packages(),
-    setup_requires=['fdint', 'numpy', 'scipy', 'pandas'],
     install_requires=[
         'fdint',
         'scipy',
@@ -29,6 +28,10 @@ setuptools.setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
