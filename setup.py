611d560f5f738ac14af7082438f2a6efc59f8009
alekseik1 <1alekseik1@gmail.com>
2020-05-10 18:09:55 +0300

    build: fix build config

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/alekseik1/ifg-py',
     packages=setuptools.find_packages(),
+    setup_requires=['fdint', 'numpy', 'scipy', ],
     install_requires=[
         'fdint',
         'scipy',
