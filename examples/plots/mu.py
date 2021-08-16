698e2c46b5c7fcd1a362453c4e7e104df847efcf
Pavel Levashov <pasha@jiht.ru>
2021-08-16 23:43:29 +0300

    Extended input
    Added arguments thetas, densities and rs besides temperatures and
    volumes. All arguments to the constructor are named (kwargs). Added
    arguments checking. Corrected examples.py and plots.
    Parameter thetas is the ratio T/T_F. rs is the Brueckner parameter:
    4./3. * \pi * r_s^3 = 1/n.

--- examples/plots/mu.py
+++ examples/plots/mu.py
@@ -1,50 +1,45 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
 from ifg import IfgCalculator
 
-matplotlib.rcParams["font.family"] = "Times New Roman"
-matplotlib.rcParams["mathtext.fontset"] = "cm"
-params = {
-    "text.usetex": True,
-    "font.size": 16,
-    "font.family": "lmodern",
-}
+
+matplotlib.rcParams['font.family'] = 'Times New Roman'
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+params = {'text.usetex': True,
+          'font.size': 16,
+          'font.family': 'lmodern',
+          }
 plt.rcParams.update(params)
 
 g = 2
-temps = np.logspace(-2, 4, num=100)
-temps10_high = np.logspace(-2, 2, num=100)
-temps10_low = np.logspace(-2, 0.7)
+#temps = np.logspace(-2, 4, num=100)
+thetas = [0.5, 1., 2.]
+# temps10_high = np.logspace(-2, 2, num=100)
+# temps10_low = np.logspace(-2, 0.7)
 
-vols = [0.1, 1, 10]
-t = (
-    IfgCalculator()
-    .with_temperatures(temps, in_si=False)
-    .with_volumes(vols, in_si=False)
-    .with_output_in_si(False)
-)
+# vols = [0.1, 1., 10.]
+vols = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0]
+#t = IfgCalculator(volumes=vols, densities = [1., 2., 3.], temperatures=temps, input_in_si=False, output_in_si=False)
+t = IfgCalculator(rs=vols, thetas=thetas)
 
-vg = 10.0
-e_fermi = np.power(3.0 * np.square(np.pi) / np.sqrt(2.0) / g / vg, 2.0 / 3.0)
-asymp1_low = e_fermi * (1.0 - np.pi * np.pi / 12.0 * np.square(temps10_low / e_fermi))
-mu_b = temps10_high * np.log(1.0 / g / vg * np.power(2.0 * np.pi / temps10_high, 1.5))
-asymp1_high = mu_b + temps10_high * np.power(np.pi, 1.5) / g / vg / np.power(
-    temps10_high, 1.5
-)
+vg = 10.
+e_fermi = np.power(3. * np.square(np.pi) / np.sqrt(2.) / g / vg, 2./3.)
+#asymp1_low = e_fermi * (1. - np.pi * np.pi / 12. * np.square(temps10_low / e_fermi))
+#mu_b = temps10_high * np.log(1. / g / vg * np.power(2. * np.pi / temps10_high, 1.5))
+#asymp1_high = mu_b + temps10_high * np.power(np.pi, 1.5) / g / vg / np.power(temps10_high, 1.5)
 
-plt.plot(temps, t.mu[:, 0], "k-", label=r"{$v = 0.1$}", linewidth=2)
-plt.plot(temps, t.mu[:, 1], "k--", label=r"{$v = 1$}", linewidth=2)
-plt.plot(temps, t.mu[:, 2], "k-.", label=r"{$v = 10$}", linewidth=2)
-plt.plot(temps10_low, asymp1_low, "b-", linewidth=1, label=r"low-$T$ asymp.")
-plt.plot(temps10_high, asymp1_high, "r-", linewidth=1, label=r"high-$T$ asymp.")
+plt.plot(vols, t.E[0, :], 'k-', label=r'{$\theta = 0.5$}', linewidth=2)
+plt.plot(vols, t.mu[1, :], 'k--', label=r'{$v = 1$}', linewidth=2)
+plt.plot(vols, t.mu[2, :], 'k-.', label=r'{$v = 2$}', linewidth=2)
+# plt.plot(temps10_low, asymp1_low, 'b-', linewidth=1, label=r'low-$T$ asymp.')
+# plt.plot(temps10_high, asymp1_high, 'r-', linewidth=1, label=r'high-$T$ asymp.')
 
-plt.xscale("log")
-plt.xlabel(r"{\LARGE $T$, Ha}")
-plt.ylabel(r"{\LARGE $\mu$")
-plt.xlim(1e-2, 1e2)
-plt.ylim(-30, 30)
-plt.legend(loc="lower left")
-plt.savefig("mu.pdf", bbox_inches="tight")
+#plt.yscale('log')
+plt.xlabel(r'{\LARGE $r_s$}')
+plt.ylabel(r'{\LARGE $\mu$')
+#plt.xlim(0, 10)
+plt.ylim(-10, 25)
+plt.legend(loc='lower left')
+plt.savefig('mu.pdf', bbox_inches='tight')
 plt.show()
--- ifg/__init__.py
+++ ifg/__init__.py
@@ -1,4 +1,4 @@
-"""Thermodynamic properties of ideal Fermi-gas.
+"""Thermodynamic properties of ideal Fermi-gas
 
 The module calculate all thermodynamic properties of ideal Fermi-gas,
 including second derivatives of thermodynamic potential. The module
@@ -8,7 +8,9 @@ import ifg
 vols = [1./1.e29]      # Array of volumes (in SI units, m^3)
 temps = [1.e3, 1.e4]   # Array of temperatures (in SI units, K)
 # Initialization, input and output in SI (False means atomic units)
-t = ifg.IfgCalculator(vols, temps, input_in_si=True, output_in_si=True)
+t = ifg.IfgCalculator(volumes=vols, temperatures=temps, input_in_si=True, output_in_si=True)
+# Calculate all available properties; if only one property is required
+# it can be called directly, for example, t.P returns pressures
 t.get_all_properties()
 # Output: E - energy (J/m^3), S - entropy (J/K/m^3),
 # P - pressure (Pa), C_V - isochoric heat capacity (J/K/m^3),
@@ -19,12 +21,11 @@ t.E[1, 0]
 """
 
 from ifg.calculator import IfgCalculator
-from ifg.metadata import __version__
 from ifg.units_converter import SiAtomicConverter, get_metal_specific_volume
 
+from ifg.metadata import __version__
+
 __all__ = [
-    "IfgCalculator",
-    "SiAtomicConverter",
-    "get_metal_specific_volume",
-    "__version__",
+    'IfgCalculator', 'SiAtomicConverter',
+    'get_metal_specific_volume', '__version__'
 ]
