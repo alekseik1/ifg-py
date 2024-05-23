8c084b69990ff13311761845e57bd25067ed0042
alekseik1 <1alekseik1@gmail.com>
2024-05-23 21:48:54 +0300

    test: fix rtol

--- tests/unit/test_around_threshold.py
+++ tests/unit/test_around_threshold.py
@@ -29,4 +29,4 @@ def test_left_right_limits_are_equal(temps_left, temps_right, vols):
         right_entropy = calc2.S
     np.testing.assert_allclose(left_C_V, right_C_V, rtol=1e-5)
     np.testing.assert_allclose(left_C_P, right_C_P, rtol=1e-5)
-    np.testing.assert_allclose(left_entropy, right_entropy, rtol=1e-6)
+    np.testing.assert_allclose(left_entropy, right_entropy, rtol=1e-5)
